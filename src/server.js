cc6b507fbd5c4bcbfbb22d1db1c91e8aef986b51
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-03-10 17:36:17 -0500

    break up long running string

--- src/server.js
+++ src/server.js
@@ -14,7 +14,12 @@ function handleResponse(req, res) {
     response = parseUnix(obj);
   if (obj.pathname === '/')
     response =
-      `This is an API timestamp service. Call the API using /api/parsetime or /api/unixtime. For example : /api/parsetime?iso=2013-08-10T12:10:15.474Z. For parsetime you will receive: { "hour":12, "minute":10, "second":15 }. For unixtime you will receive: { "unix":1376136615474 }. For an invalid ISO 8601 format you will receive: { error: 'Invalid ISO-format' }. For any questions contact Adam Recvlohe <adam.recvlohe@gmail.com>`;
+      'This is an API timestamp service. Call the API using /api/parsetime or /api/unixtime.' +
+      ' For example : /api/parsetime?iso=2013-08-10T12:10:15.474Z.' +
+      ' For parsetime you will receive: { "hour":12, "minute":10, "second":15 }.' +
+      ' For unixtime you will receive: { "unix":1376136615474 }.' +
+      ' For an invalid ISO 8601 format you will receive: { error: "Invalid ISO-format" }.' +
+      ' For any questions contact Adam Recvlohe <adam.recvlohe@gmail.com>';
 
   if (response) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
